391b5862a189338556ba254fcf9977d605c7b6fd
Manasi <manasi@openbrix.co.uk>
2022-11-08 11:39:09 +0530

    functions

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function getUsers (req, res) {
     let data = fs.readFileSync('users.json')
-    data = JSOn.parse (data);
+    data = JSON.parse (data);
     res.send (data);
 }
 
@@ -16,22 +16,54 @@ function getUserWithFirstName (req, res) {
     res.send(usersWithFirstName);
 }
 
+function addUser(req, res) {
+    let rawdata = fs.readFileSync('users.json');
+    rawdata = JSON.parse(rawdata)
+    const idNo = req.body.mailId;
+    console.log(idNo)
+    const usersWithid = rawdata.filter((user) =>{
+        return  user.mailId == idNo;  
+     })
+     if(usersWithid.length >0){
+        res.send ("user alredy exists")
+     }
+     else {
+        rawdata.push(req.body)
+        fs.writeFileSync('users.json', JSON.stringify(rawdata));
+    res.send('Added user');
+     }  
+
+ }
+
 
 function getUserWithLasttName (req, res) {
     let data = fs.readFileSync('users.json');
     data = JSON.parse(data)
-    const firstName = req.params.fname;
+    const lastname = req.params.lname;
     const getUserWithLasttName = data.filter((user) =>{
-       return user.fname.toLocaleLowerCase() == lastname.toLowerCase();
+       return user.lname.toLocaleLowerCase() == lastname.toLowerCase();
     })
     res.send(getUserWithLasttName);
 }
 
+function deleteUser(req,res) {
+    let rawdata = fs.readFileSync('users.json');
+    rawdata = JSON.parse(rawdata)
+    const fName = req.params.fname;
+    const deleteUsersWithFirstName = rawdata.filter((user) =>{
+        return user.fname.toLocaleLowerCase() !== fName.toLowerCase();
+    })
+    fs.writeFileSync('users.json', JSON.stringify(deleteUsersWithFirstName));
+
+    res.send(deleteUsersWithFirstName);
+ }
 
 
 
 module.exports = {
     getUsers,
     getUserWithFirstName,
-    getUserWithLasttName
+    getUserWithLasttName,
+    addUser,
+    deleteUser
 }
